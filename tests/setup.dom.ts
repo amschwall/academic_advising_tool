b0d4f62127eef76e90fc4153247068ff8b7f2dcf
Abigail Schwall <amschwall@wm.edu>
2026-05-10 17:12:47 -0400

    Chore: add set up tests

---
+++ tests/setup.dom.ts
@@ -0,0 +1,5 @@
+// file: tests/setup.dom.ts
+// Extends Jest's expect with @testing-library/jest-dom matchers
+// (toBeInTheDocument, toBeVisible, toHaveValue, etc.)
+
+import "@testing-library/jest-dom";
